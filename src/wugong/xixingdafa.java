955354cda4357619b781021471b922c94f4a319d
yuan <1940944942@qq.com>
2020-03-23 14:01:37 +0800

    xinwugong

---
+++ src/wugong/xixingdafa.java
@@ -0,0 +1,13 @@
+package wugong;
+
+public class xixingdafa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1111);
+		System.out.println(1111);
+		System.out.println(1111);
+		System.out.println(1111);
+	}
+
+}
